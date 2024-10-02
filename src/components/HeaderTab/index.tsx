55105924f31d9de2dc9ef21efcbeb7572ec7bb3c
morhoangtv <truonghoang14122003@gmail.com>
2024-10-03 00:48:17 +0700

    fix time issue

---
+++ src/components/HeaderTab/index.tsx
@@ -0,0 +1,16 @@
+import {View} from 'react-native';
+import React from 'react';
+import {ScaledSheet} from 'react-native-size-matters/extend';
+import Typo from '../Typo';
+
+const HeaderTab = () => {
+  return (
+    <View>
+      <Typo></Typo>
+    </View>
+  );
+};
+
+export default HeaderTab;
+
+const styles = ScaledSheet.create({});
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,15 +4,30 @@ import moment from 'moment';
 export const getDaySession = () => {
   const now = moment().format('YYYY-MM-DDTHH:mm:ss');
   const currentDate = moment(now).format('YYYY-MM-DD');
-  const currentYesterday = moment(now).subtract(1, 'days').format('YYYY-MM-DD');
+  const currentDateYesterday = moment(now)
+    .subtract(1, 'days')
+    .format('YYYY-MM-DD');
+  const currentDateTomorrow = moment(now).add(1, 'days').format('YYYY-MM-DD');
+  let isBeforeMidNight: boolean;
+  const currentHours = moment(now).hours();
+  if (currentHours >= 23) {
+    isBeforeMidNight = true;
+  } else if (currentHours >= 0 && currentHours < 4) {
+    isBeforeMidNight = false;
+  }
+
   let result = '';
   sessions.forEach(item => {
     const startTime =
-      (item === 'night' ? currentYesterday : currentDate) +
+      (item === 'night' && !isBeforeMidNight
+        ? currentDateYesterday
+        : currentDate) +
       'T' +
       sessionTime[`${item}StartHour` as keyof typeof sessionTime];
     const endTime =
-      currentDate +
+      (item === 'night' && isBeforeMidNight
+        ? currentDateTomorrow
+        : currentDate) +
       'T' +
       sessionTime[`${item}EndHour` as keyof typeof sessionTime];
     const start = moment(startTime);
