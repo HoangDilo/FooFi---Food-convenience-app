b917d2a88f5aa6a5913ed29a8de544101f7369a6
HoangDilo <truonghoang14122003@gmail.com>
2024-10-24 17:34:54 +0700

    update authen

---
+++ src/screens/My/Authentication/SignUp/index.tsx
@@ -0,0 +1,109 @@
+import {TextInput, TouchableHighlight, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {ScaledSheet} from 'react-native-size-matters/extend';
+import Typo from '@/components/Typo';
+import colorsConstant from '@/constants/colors.constant';
+import {useTranslation} from 'react-i18next';
+
+const INPUTS = [
+  {
+    key: 'email',
+    content_type: 'emailAddress',
+    placeholder_key: 'email_placeholder',
+    secureTextEntry: false,
+  },
+  {
+    key: 'name',
+    content_type: 'emailAddress',
+    placeholder_key: 'name_placeholder',
+    secureTextEntry: false,
+  },
+  {
+    key: 'password',
+    content_type: 'emailAddress',
+    placeholder_key: 'password_placeholder',
+    secureTextEntry: true,
+  },
+];
+
+const SignIn = () => {
+  const {t} = useTranslation();
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSignIn = useCallback(() => {}, []);
+
+  return (
+    <View style={styles.loginForm}>
+      {INPUTS.map(input => (
+        <View key={input.key}>
+          <Typo style={styles.label}>{t(`my.${input.key}`)}</Typo>
+          <TextInput
+            style={styles.input}
+            placeholder={t(`my.${input.placeholder_key}`)}
+            placeholderTextColor={colorsConstant.skeleton_1}
+            cursorColor={colorsConstant.primary}
+            secureTextEntry={input.secureTextEntry}
+            textContentType={input.content_type as any}
+          />
+        </View>
+      ))}
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor={colorsConstant.primary_press}
+        onPress={handleSignIn}>
+        <Typo style={styles.buttonLabel}>{t('my.sign_in')}</Typo>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default SignIn;
+
+const styles = ScaledSheet.create({
+  loginForm: {
+    paddingHorizontal: '24@s',
+    paddingVertical: '36@s',
+    borderRadius: '16@s',
+    shadowColor: colorsConstant.shadow,
+    backgroundColor: '#fff',
+    elevation: 4,
+    width: '82%',
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    alignSelf: 'center',
+    gap: '20@s',
+  },
+  input: {
+    borderWidth: '1@s',
+    borderColor: colorsConstant.skeleton_1,
+    borderRadius: 999,
+    paddingVertical: '8@s',
+    paddingHorizontal: '16@s',
+    fontSize: '14@s',
+    fontWeight: '500',
+  },
+  label: {
+    color: colorsConstant.black_1,
+    fontWeight: '500',
+    fontSize: '16@s',
+    marginBottom: '6@s',
+    marginLeft: '6@s',
+  },
+  button: {
+    backgroundColor: colorsConstant.primary,
+    borderRadius: 999,
+    paddingVertical: '12@s',
+    marginTop: '6@s',
+    alignItems: 'center',
+  },
+  buttonLabel: {
+    fontWeight: '700',
+    color: '#FFF',
+    fontSize: '18@s',
+  },
+});
--- src/screens/My/Authentication/index.tsx
+++ src/screens/My/Authentication/index.tsx
@@ -1,23 +1,54 @@
-import {View} from 'react-native';
-import React, {useState} from 'react';
+import {Pressable, ScrollView, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
 import Typo from '@/components/Typo';
-import {ScaledSheet} from 'react-native-size-matters/extend';
+import {scale, ScaledSheet} from 'react-native-size-matters/extend';
 import colorsConstant from '@/constants/colors.constant';
 import {useTranslation} from 'react-i18next';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import IconXML from '@/components/IconXML';
+import Google from '@/assets/icons/Google';
 
 const Authentication = () => {
   const {t} = useTranslation();
 
-  const [type, setType] = useState('login');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleSwitchSignUp = useCallback(() => {
+    setIsLogin(!isLogin);
+  }, [isLogin]);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container} keyboardShouldPersistTaps="handled">
       <View style={styles.titleBox}>
-        <Typo style={styles.title}>{t('my.sign_in')}</Typo>
-        <Typo style={styles.description}>{t('my.sign_in_description')}</Typo>
+        <Typo style={styles.title}>
+          {t(isLogin ? 'my.sign_in' : 'my.sign_up')}
+        </Typo>
+        <Typo style={styles.description}>
+          {t(isLogin ? 'my.sign_in_description' : 'my.sign_up_description')}
+        </Typo>
       </View>
-      <View style={styles.loginForm}></View>
-    </View>
+      <View style={styles.floatView}>
+        {isLogin ? <SignIn /> : <SignUp />}
+        <View style={styles.googleSignIn}>
+          <View style={styles.orWrapper}>
+            <View style={styles.line} />
+            <Typo style={styles.or}>{t('my.or')}</Typo>
+            <View style={styles.line} />
+          </View>
+          <Pressable style={styles.googleButton}>
+            <IconXML icon={Google} width={scale(32)} height={scale(32)} />
+            <Typo style={styles.googleText}>{t('my.sign_in_google')}</Typo>
+          </Pressable>
+        </View>
+        <Typo style={styles.dontHaveAccount}>
+          {t(isLogin ? 'my.dont_have_account' : 'my.already_have_account')}{' '}
+          <Typo style={styles.createAccount} onPress={handleSwitchSignUp}>
+            {t(isLogin ? 'my.create_account' : 'my.sign_in')}
+          </Typo>
+        </Typo>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -44,7 +75,60 @@ const styles = ScaledSheet.create({
     color: '#FFF',
     fontSize: '16@s',
   },
-  loginForm: {
-    padding: '24@s',
+  floatView: {
+    transform: [
+      {
+        translateY: -68,
+      },
+    ],
+    alignItems: 'center',
+  },
+  googleSignIn: {
+    marginTop: '20@s',
+    width: '82%',
+  },
+  or: {
+    color: colorsConstant.gray_2,
+    fontSize: '16@s',
+    fontWeight: '500',
+    marginHorizontal: '8@s',
+  },
+  line: {
+    backgroundColor: colorsConstant.skeleton_1,
+    height: '1@s',
+    flex: 1,
+  },
+  orWrapper: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+  },
+  googleButton: {
+    borderWidth: '1@s',
+    borderColor: colorsConstant.primary,
+    borderRadius: 999,
+    paddingHorizontal: '6@s',
+    paddingVertical: '8@s',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: '16@s',
+    alignItems: 'center',
+    gap: '12@s',
+    backgroundColor: colorsConstant.primary_fade,
+    marginHorizontal: '20@s',
+  },
+  googleText: {
+    fontSize: '18@s',
+    color: '#000',
+    fontWeight: '500',
+  },
+  dontHaveAccount: {
+    marginTop: '24@s',
+    color: colorsConstant.black_2,
+  },
+  createAccount: {
+    color: colorsConstant.primary,
+    textDecorationLine: 'underline',
+    fontWeight: '500',
   },
 });
